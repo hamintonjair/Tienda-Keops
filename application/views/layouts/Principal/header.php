99d73fca229fd314ccc09d1bfcde852e2597baad
Haminton Mena Mena <hamintonjair@gmail.com>
2023-02-05 21:05:29 -0500

    tidio

--- application/views/layouts/Principal/header.php
+++ application/views/layouts/Principal/header.php
@@ -104,6 +104,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css">
+
 <!-- scrip para el boot -->
 <script src="//code.tidio.co/he656z7p8gra0kt1f8pg3wj5wgxcqed8.js" async></script>
 
